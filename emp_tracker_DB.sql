9b3f7377fcf22164af017baa5e30f61384054876
Kogan <koganp42@gmail.com>
2020-01-30 11:29:48 -0600

    Added delete cascade to all foreign keys

--- emp_tracker_DB.sql
+++ emp_tracker_DB.sql
@@ -9,11 +9,11 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT NULL,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -22,8 +22,8 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
 INSERT INTO department (name)
 VALUES ("Sales"), ("Marketing"), ("Quality Assurance"), ("Legal"), ("Development");
