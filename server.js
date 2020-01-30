3646e5d179b59ee3fda3a6c11d65560a73c7dae4
Kogan <koganp42@gmail.com>
2020-01-30 12:50:47 -0600

    Add new employee and update role both working now

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const connection = mysql.createConnection({
 });
 //left, inner, inner join for getting the employees and all relevant information
 //Select e.id, e.name, m.id, m.name FROM employee as e LEFT JOIN employee on e.manager=m.id
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log(`Your port is ${process.env.PORT}`);
   startPrompts();
@@ -39,53 +39,53 @@ function startPrompts() {
         "Exit"
       ]
     })
-    .then(function(answer) {
+    .then(function (answer) {
       switch (answer.action) {
 
-      case "View all departments":
-        viewDepartments();
-        break;
+        case "View all departments":
+          viewDepartments();
+          break;
 
-      case "View all roles":
-        viewRoles();
-        break;
-      
-      case "View all employees":
-        viewEmployees();
-        break;
+        case "View all roles":
+          viewRoles();
+          break;
 
-      case "Add a new department":
-        addNewDepartment();
-        break;
+        case "View all employees":
+          viewEmployees();
+          break;
 
-      case "Add a new role":
-        addNewRole();
-        break;
+        case "Add a new department":
+          addNewDepartment();
+          break;
 
-      case "Add a new employee":
-        addNewEmployee();
-        break;
+        case "Add a new role":
+          addNewRole();
+          break;
 
-      case "Update an employee's role":
-        updateEmployeeRole();
-        break;
+        case "Add a new employee":
+          addNewEmployee();
+          break;
 
-      case "Exit":
-        connection.close(function(err) {
-          if (err) {
-            console.log('Error closing connection', err);
-          } else {
-            console.log('Connection closed');
-          }
-        });
-    }
+        case "Update an employee's role":
+          updateEmployeeRole();
+          break;
+
+        case "Exit":
+          connection.close(function (err) {
+            if (err) {
+              console.log('Error closing connection', err);
+            } else {
+              console.log('Connection closed');
+            }
+          });
+      }
     });
 }
 
 //The following three functions query the mysql database for the specific table data being requested.
 function viewDepartments() {
   let query = "SELECT * FROM department";
-  connection.query(query, function(err, res) {
+  connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
     startPrompts();
@@ -94,7 +94,7 @@ function viewDepartments() {
 
 function viewRoles() {
   let query = "SELECT * FROM role";
-  connection.query(query, function(err, res) {
+  connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
     startPrompts();
@@ -103,7 +103,7 @@ function viewRoles() {
 
 function viewEmployees() {
   let query = "SELECT * FROM employee";
-  connection.query(query, function(err, res) {
+  connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
     startPrompts();
@@ -112,142 +112,105 @@ function viewEmployees() {
 
 // The inquirer function called when a new department is being added.
 function addNewDepartment() {
-    inquirer
-      .prompt({
-        name: "newDepartmentName",
-        type: "input",
-        message: "What is the name of the new department?",
-        validate: validatePrompt.validateRequiredTextInput
-      })
-      .then(function(answer) {
-        let regularAnswer = answer.newDepartmentName;
-        let lowerCaseAnswer = regularAnswer.toLowerCase();
-        connection.query("SELECT LOWER(name) AS nameL FROM department", function(err, res) {
-          if (err) throw err;
-          res.forEach(nameL => {
-            if (lowerCaseAnswer === nameL) {
-              return new Error ("Sorry, that department already exists!");
-            }
-          });
-          connection.query(
-            "INSERT INTO department SET ?",
-            {
-              name: regularAnswer,
-            },
-            function(err) {
-              if (err) throw err;
-              console.log("The new department was added succesfully!");
-              startPrompts();
-            }
-          );
+  inquirer
+    .prompt({
+      name: "newDepartmentName",
+      type: "input",
+      message: "What is the name of the new department?",
+      validate: validatePrompt.validateRequiredTextInput
+    })
+    .then(function (answer) {
+      let regularAnswer = answer.newDepartmentName;
+      let lowerCaseAnswer = regularAnswer.toLowerCase();
+      connection.query("SELECT LOWER(name) AS nameL FROM department", function (err, res) {
+        if (err) throw err;
+        //The validation below currently doesn't work
+        res.forEach(nameL => {
+          if (lowerCaseAnswer === nameL) {
+            console.log("")
+            return new Error("Sorry, that department already exists!");
+          }
         });
+        connection.query(
+          "INSERT INTO department SET ?",
+          {
+            name: regularAnswer,
+          },
+          function (err) {
+            if (err) throw err;
+            console.log("The new department was added succesfully!");
+            startPrompts();
+          }
+        );
       });
-      
-  }
+    });
+
+}
 
 // The inquirer function called when a new role is being added.
 function addNewRole() {
   let departmentChoices = [];
-  connection.query("SELECT name FROM department", function(err, res) {
+  connection.query("SELECT name, id FROM department", function (err, res) {
     if (err) throw err;
-    res.forEach(departmentName => {
-      departmentChoices.push(departmentName);
+    res.forEach(department => {
+      departmentChoices.push(department);
     })
-  inquirer
-    .prompt([
-      {
-        name: "newRoleDepartment",
-        type: "rawlist",
-        message: "Which department does the new role belong to?",
-        choices: departmentChoices
-      },
-      {
-      name: "newRoleTitle",
-      type: "input",
-      message: "What is the title of the new role?",
-      validate: (answer) => {
-        if (typeof answer === "string" && answer.length > 1) {
-          return true;
-        }
-        return false;
-      }
-      },
-      {
-        name: "newRoleSalary",
-        type: "input",
-        message: "What is the salary for the new role?",
-        validate: function(answer) {
-          if (typeof answer === "number") {
-            return true;
-          } else {
-            return false
-          };
-        }
-      }
-  ])
-    .then(function(answer) {
-      connection.query("SELECT LOWER(name FROM department", function(err, res) {
-        res.forEach(role => {
-          if (answer.newRoleTitle === role.name) {
-            return true;
-          }
-          return false;
-        });
-      });
-      connection.query(
-        "INSERT INTO role SET ?",
+    console.table(res);
+    inquirer
+      .prompt([
         {
-          title: answer.newRoleTitle,
-          salary: answer.newRoleSalary, 
-          department_id: answer.newRoleDepartment || 0
+          name: "newRoleDepartment",
+          type: "number",
+          message: "What is the id of the department the new role will belong to?",
+          // Might try to come back and make the rawlist method work instead, but for now going with a simple input.
+          //type: "rawlist",
+          //message: "Which department does the new role belong to?",
+          //choices: departmentChoices
         },
-        function(err) {
-          if (err) throw err;
-          console.log("The new department was added succesfully!");
-          startPrompts();
-        }
-      );
-    });
-  });
-}
-
-// The inquirer function called when a new role is being added.
-function addNewEmployee() {
-  inquirer
-    .prompt([
-      {
-      name: "newEmployeeFirstName",
-      type: "input",
-      message: "What is the employee's first name?",
-      validate: (answer) => {
-        if (typeof answer === "string" && answer !== null) {
-          return true;
-        }
-        return false;
-      }
-      },
-      {
-        name: "newEmployeeLastName",
-        type: "input",
-        message: "What is the employee's first name?",
-        validate: function(answer) {
-          if (typeof answer === "number") {
-            return true;
-          } else {
-            return false
-          };
-        }
-      },
-      {
-        name: "newEmployeeRole",
-        type: "input",
-        message: "What is the id number of the department the new role belongs to?",
-        validate: (answer) => {
-          if (typeof answer === "number") {
-            return true;
+        {
+          name: "newRoleTitle",
+          type: "input",
+          message: "What is the title of the new role?",
+          validate: (answer) => {
+            if (typeof answer === "string" && answer.length > 1) {
+              return true;
+            }
+            return false;
           }
-          return false;
+        },
+        {
+          name: "newRoleSalary",
+          type: "number",
+          message: "What is the salary for the new role?",
+          // validate: function(answer) {
+          //   if (typeof answer === "number") {
+          //     return true;
+          //   } else {
+          //     return false
+          //   };
+          // }
         }
+      ])
+      .then(function (answer) {
+        // connection.query("SELECT * FROM role LEFT JOIN department ON role.department WHERE title = ? AND ", function(err, res) {
+        //   if (err){
+        connection.query(
+          "INSERT INTO role SET ?",
+          {
+            title: answer.newRoleTitle,
+            salary: answer.newRoleSalary,
+            department_id: answer.newRoleDepartment || 0
+          },
+          function (err) {
+            if (err) throw err;
+            console.log("The new role was added succesfully!");
+            startPrompts();
+          }
+        );
+        // } else {
+        //   return
+        // }
+        //});
         // The below checks to see whether there's already a role of the same name in the given department, preventing duplicates from being added. I'd like to learn enough of regex to make the check more complete by making the check case insensitive. Also, this may need to be moved to another part of the inquirer prompt as I'll need to check
         // validate: function(answer) {
         //   let query = "SELECT * FROM role";
@@ -259,29 +222,102 @@ function addNewEmployee() {
         //       }
         //       return false;
         //     });
-            
+
         //   });
         // }
+      });
+  });
+}
+
+// The inquirer function called when a new role is being added.
+function addNewEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "newEmployeeFirstName",
+        type: "input",
+        message: "What is the employee's first name?",
+        validate: (answer) => {
+          if (typeof answer === "string" && answer !== null) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "newEmployeeLastName",
+        type: "input",
+        message: "What is the employee's last name?",
+      },
+      {
+        name: "newEmployeeRole",
+        type: "number",
+        message: "What is the id number of the employee's new role?",
+        validate: (answer) => {
+          if (typeof answer === "number") {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "newEmployeeManagerConfirm",
+        type: "confirm",
+        message: "Does the new employee have a manager?",
+      },
+      {
+        name: "newEmployeeManager",
+        type: "number",
+        message: "What is the id number of the employee's new manager?",
+        when: function (answer) {
+          return answer.newEmployeeManagerConfirm === true;
+        }
       }
-  ])
-    .then(function(answer) {
+    ])
+    .then(function (answer) {
       connection.query(
-        "INSERT INTO role SET ?",
+        "INSERT INTO employee SET ?",
         {
-          title: answer.newRoleTitle,
-          salary: answer.newRoleSalary, 
-          department_id: answer.newRoleDepartment || 0
+          first_name: answer.newEmployeeFirstName,
+          last_name: answer.newEmployeeLastName,
+          role_id: answer.newEmployeeRole || 0,
+          manager_id: answer.newEmployeeManager || null
         },
-        function(err) {
+        function (err) {
           if (err) throw err;
-          console.log("The new department was added succesfully!");
+          console.log("The new employee was added succesfully!");
           startPrompts();
         }
       );
     });
 }
 
+function updateEmployeeRole() {
+  inquirer
+    .prompt([
+      {
+        name: "selectEmployee",
+        type: "number",
+        message: "What is the id of the employee whose role you want to edit?"
+      },
+      {
+        name: "selectNewRole",
+        type: "number",
+        message: "What is the id of the employee's new role?"
+      }      
+    ])
+      .then(function (answer) {
+        connection.query("UPDATE employee AS e SET e.role_id = ? WHERE e.id = ?", [answer.selectNewRole, answer.selectEmployee], function (err, res) {
+          if (err) throw err;
+          console.log("The employee's role was changed succesfully!");
+          startPrompts();
+        });
+      });
+
+  }
 
 
 
-// If you need to delete a department or role, or an employee who manages another, you'll need to casace the delete
+
+
+// If you need to delete a department or role, or an employee who manages another, you'll need to casade the delete
